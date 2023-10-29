2433c9a96468b427d449363ed948791b92156436
Stanislav20 <stas_on38@mail.ru>
2023-10-29 21:39:45 +0800

    mars

---
+++ src/scripts/script.js
@@ -0,0 +1,28 @@
+  const buttonElem = document.querySelector('.header__btn');
+  const modalElem = document.querySelector('.modal');
+
+	modalElem.style.cssText = `
+	display: flex;
+	visibility: hidden;
+	opacity: 0;
+	transition: opacity 300ms ease-in-out;
+`;
+
+  const closeModal = event => {
+    const target = event.target;
+
+    if (target === target.closest('.modal__close')) {
+			modalElem.style.opacity = 0;
+      setTimeout(() => {
+        modalElem.style.visibility = 'hidden';
+      }, 300);
+    }
+  }
+
+  const openModal = () => {
+    modalElem.style.visibility = 'visible';
+    modalElem.style.opacity = 1;
+  };
+
+buttonElem.addEventListener('click', openModal);
+modalElem.addEventListener('click', closeModal);
